d3775f2b47d85388e49dfd88720ca07b673ad541
koharakouki <ko.kohara02@gmail.com>
2020-07-04 17:11:48 +0900

    フォームの利用

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 
 class App extends Component {
-  input = '';
+  input = ' ';
 
   msgStyle = {
   	fontSize: "20pt",
@@ -13,14 +13,18 @@ class App extends Component {
   	margin: "20px 0px",
   	padding: "5px",
   }
+  inputStyle = {
+  	fontsize: "12pt",
+  	padding: "5px",
+  }
 
   constructor(props){
   	super(props);
-  	this.state = {
-  		massage: "type your name:"
-  	};
-  	this.doChange = this.doChange.bind(this);
-  	this.doSubmit = this.doSubmit.bind(this);
+    this.state = {
+    	message: "type your name:"
+    };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
   }
 
   doChange(event){
@@ -38,43 +42,17 @@ class App extends Component {
   	return(
   		<div>
   		  <h1>React</h1>
-  		  <Message title="Children!">
-  		    これはコンポーネント内のコンテンツです。
-  		    まるでテキストを分割し、リストにして表示します。
-  		    改行は必要ありません。
-  		  </Message>
+  		  <h2>{this.state.message}</h2>
+  		  <form onSubmit={this.doSubmit}>
+  		    <label>
+  		      <span style={this.inputStyle}></span>Message:
+  		      <input type="text" style={this.inputStyle} onChange={this.doChange} />
+  		    </label>
+  		    <input type="submit" style={this.inputStyle} value="Click" />
+  		  </form>
   		</div>
   	);
   }
-}
-
-class Message extends Component {
-	li = {
-		fontSize: "16px",
-		color: "#06",
-		margin: "0px",
-		padding: "0px",
-	}
-
-	render(){
-		let content = this.props.children;
-		let arr = content.split('。');
-		let arr2 = [];
-		for(let i = 0; i < arr.length; i++){
-			if(arr[i].trim() != ''){
-				arr2.push(arr[i]);
-			}
-		}
-		let list = arr2.map((value, key)=>(
-			<li style={this.li} key={key}>{value}.</li>
-		));
-		return (
-			<div>
-			  <h2>{this.props.title}</h2>
-			  <ol>{list}</ol>
-			</div>
-		);
-	}
 }
 
 export default App;
