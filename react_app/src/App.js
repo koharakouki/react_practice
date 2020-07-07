623ff9f9eec2d5f9be5e4fd062f3cb427c95a18d
koharakouki <ko.kohara02@gmail.com>
2020-07-08 06:27:29 +0900

    Appコンポーネント

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,53 +1,13 @@
 import React, { Component } from 'react';
-import Rect from './Rect';
 import './App.css';
 
-let theme = {
-	light:{
-		backgroundColor: "#eef",
-		color: "#006",
-		padding: "10px",
-	},
-	dark:{
-		backgroundColor: "#006",
-		color: "#eef",
-		padding: "10px",
-	}
-};
-
-const ThemeContext = React.createContext(theme);
-
 class App extends Component {
-	static contextType = ThemeContext;
-
 	render(){
 		return(
-			<div style={this.context.light}>
-			  <Title value="Content page" />
-			  <Message value="This is Content sample." />
-			  <Message value="※これはテーマのサンプルです。" />
+			<div>
+			  <h1>React</h1>
+			  <p>This is sample component. </p>
 			</div>
-		);
-	}
-}
-
-
-class Title extends Component {
-	static contextType = ThemeContext;
-
-	render(){
-		return(
-		<h2 style={this.context.light}>{this.props.value}</h2>
-		);
-	}
-}
-
-class Message extends Component {
-	static contextType = ThemeContext;
-
-	render(){
-		return(
-			<p style={this.context.dark}>{this.props.value}</p>
 		);
 	}
 }
