f5f85e48588a3b45287811e712e1984d1364264f
koharakouki <ko.kohara02@gmail.com>
2020-07-10 19:38:28 +0900

    ストア作成

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+// ステートの値
+let state_value = {
+	counter: 0,
+	message: "COUNTER"
+}
+
+function counter(state = state.value, action) {
+	switch (action.type) {
+		case 'INCREMENT':
+		return {
+			counter: state_value + 1,
+			message: 'INCREMENT'
+		};
+		case 'DECREMENT':
+		return {
+			counter: state_value - 1,
+			message: 'DECREMENT'
+		};
+		default:
+		return state;
+	}
+}
+
+//ストア作成
+let store = createStore(counter);
+
+//表示をレンダリング
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+	  <App />
+  </Provider>,
   document.getElementById('root')
 );
 
