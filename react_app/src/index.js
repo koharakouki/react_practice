14015195aca0a6174299f08b0ceb155b4051f46e
koharakouki <ko.kohara02@gmail.com>
2020-07-12 00:06:37 +0900

    永続化

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -7,44 +7,45 @@ import storage from 'redux-persist/lib/storage'
 import { persistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
+import MemoStore, { memoReducer } from './memo/Store'
 
 
-let state_value = {
-	counter: 0,
-	message: "COUNTER"
-}
+// let state_value = {
+// 	counter: 0,
+// 	message: "COUNTER"
+// }
 
 
-function counter(state = state_value, action){
-	switch(action.type){
-		case 'INCREMENT':
-		return {
-			counter: state.counter + 1,
-			message: 'INCREMENT'
-		};
-		case 'DECREMENT':
-		return {
-			counter: state.counter - 1,
-			message: 'DECREMENT'
-		};
-		case 'RESET':
-		return {
-			counter: 0,
-			message: "RESET"
-		};
-		default:
-		return state;
-	}
-}
+// function counter(state = state_value, action){
+// 	switch(action.type){
+// 		case 'INCREMENT':
+// 		return {
+// 			counter: state.counter + 1,
+// 			message: 'INCREMENT'
+// 		};
+// 		case 'DECREMENT':
+// 		return {
+// 			counter: state.counter - 1,
+// 			message: 'DECREMENT'
+// 		};
+// 		case 'RESET':
+// 		return {
+// 			counter: 0,
+// 			message: "RESET"
+// 		};
+// 		default:
+// 		return state;
+// 	}
+// }
 
 // Redux Persistの設定
 const persistConfig= {
-	key: 'root',
+	key: 'memo',
 	storage,
 }
 
 // パーシストレデューサーの作成
-const persistedReducer = persistReducer(persistConfig, counter)
+const persistedReducer = persistReducer(persistConfig, memoReducer)
 
 // ストア、パーシスターの作成
 let store = createStore(persistedReducer)
@@ -53,9 +54,11 @@ let pstore = persistStore(store)
 // 表示をレンダリング
 ReactDOM.render(
 	<Provider store={store}>
-	  <persistGate loading={<p>loading</p>} persister={pstore}>
+	  <persistGate loading={<p>loading...</p>} persister={pstore}>
 	    <App />
 	  </persistGate>
 	</Provider>,
 	document.getElementById('root')
 );
+
+export default pstore;
