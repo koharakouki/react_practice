ab00559b4abc5ba74fc5bae0396cd5602a753ff3
koharakouki <ko.kohara02@gmail.com>
2020-07-11 14:23:00 +0900

    create index.js

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { persistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
-import MemoStore from './memo/Store'
 
+
+let state_value = {
+	counter: 0,
+	message: "COUNTER"
+}
+
+
+function counter(state = state_value, action){
+	switch(action.type){
+		case 'INCREMENT':
+		return {
+			counter: state.counter + 1,
+			message: 'INCREMENT'
+		};
+		case 'DECREMENT':
+		return {
+			counter: state.counter - 1,
+			message: 'DECREMENT'
+		};
+		case 'RESET':
+		return {
+			counter: 0,
+			message: "RESET"
+		};
+		default:
+		return state;
+	}
+}
+
+// Redux Persistの設定
+const persistConfig= {
+	key: 'root',
+	storage,
+}
+
+// パーシストレデューサーの作成
+const persistReducer = persistReducer(persistConfig, counter)
+
+// ストア、パーシスターの作成
+let store = createStore(persistReducer)
+let pstore = persistStore(store)
+
+// 表示をレンダリング
 ReactDOM.render(
-	<Provider store={MemoStore}>
-	  <App />
+	<Provider store={store}>
+	  <persistGate loading={<p>loading</p>} persister={pstore}>
+	    <App />
+	  </persistGate>
 	</Provider>,
 	document.getElementById('root')
 );
