bfd364d7727356374e7b0e16c9822f2fc714f8ed
koharakouki <ko.kohara02@gmail.com>
2020-07-16 06:26:29 +0900

    コンポーネントを組み合わせる

--- re-react/src/index.js
+++ re-react/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <App title="React" message="こんにちは" />,
+    <App />,
   document.getElementById('root')
 );
 
