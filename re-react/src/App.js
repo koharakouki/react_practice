20d671cfb73218466adedb279f2ef5681faab0a5
koharakouki <ko.kohara02@gmail.com>
2020-07-16 06:51:29 +0900

    ステートを準備する

--- re-react/src/App.js
+++ re-react/src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Rect from './Rect';
 
 class App extends Component {
+  msgStyle = {
+    fontSize: "24pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: 'Hello Component'
+    };
+  }
+
   render(){
     return(
       <div>
-        <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
-        <Rect x="150" y="100" w="150" h="150" c="magenta" r="75" />
-        <Rect x="100" y="150" w="150" h="150" c="black" r="25" />
+        <h1>React</h1>
+        <p style={this.msgStyle}>{this.state.msg}</p>
+        <p style={this.msgStyle}>{this.props.msg}</p>
       </div>
     );
   }
