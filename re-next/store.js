15e01d669536572db8315014121b8b6b39da5e18
koharakouki <ko.kohara02@gmail.com>
2020-07-20 15:07:47 +0900

    store.js修正

--- re-next/store.js
+++ re-next/store.js
@@ -4,36 +4,41 @@ import thunkMiddleware from 'redux-thunk';
 // ステート初期値
 const initial = {
 	message: 'START',
-	count: 0
+	data: [],
+	number: [],
+	result: 0
 }
 
 // レデューサー
-function conterReducer (state = initial, action) {
+function calcReducer(state= initial, action){
 	switch(action.type){
-		case 'INCREMENT':
-		return {
-			message: 'INCREMENT',
-			count: state.count + 1
-		};
-
-		case 'DECREMENT':
-		return {
-			message: 'DECREMENT',
-			count: state.count - 1
-		};
-
+		case 'ENTER':
+		  let data2 = state.data.slice();
+		  let s = action.value;
+		  data2.unshift(s);
+		  let num = s.replace(/[^0-9]/g,"");
+		  let number2 = state.number.slice();
+		  number2.unshift(num);
+		  let result = (state.result * 1) + (num * 1);
+		  return {
+		  	message: 'ENTER',
+		  	data: data2,
+		  	number: number2,
+		  	result: result
+		  };
 		case 'RESET':
-		return {
-			message: 'RESET',
-			count: initial.count
-		};
-
+		  return {
+		  	message: 'RESET',
+		  	data: [],
+		  	number: [],
+		  	result: 0
+		  };
 		default:
-		return state;
+		  return state;
 	}
 }
 
 // initStore関数(redux-store.jsで必要)
 export function initStore(state = initial){
-	return createStore(conterReducer, state, applyMiddleware(thunkMiddleware))
+	return createStore(calcReducer, state, applyMiddleware(thunkMiddleware))
 }
